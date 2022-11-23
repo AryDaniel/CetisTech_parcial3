3024f58e4d3240b1dfa269214ef66676dff36db9
AryDaniel <ary.lopez@cetis107.edu.mx>
2022-11-23 14:45:00 -0600

    Capacidad de iniciar sesión

---
+++ nuevo_usuario.php
@@ -0,0 +1,17 @@
+<form action="nuevo_usuario2.php" method="POST" style="width: 200px; margin: auto;">
+    
+
+    <label for="">Correo:</label>
+    <input type="text" name="correo" required>
+
+    <label for="">Nombre:</label>
+    <input type="text" name="nombre" required >
+
+    <label for="">Contraseña</label>
+    <input type="text" name="contrasena" required>
+
+    <br><br>
+        <button type="submit">  Crear usuario </button>
+    <br><br>
+
+</form>
