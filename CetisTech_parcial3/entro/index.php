1350c9d6195d686c8f2b4db045842261129d3971
Alexander <114307680+Alexander37ss@users.noreply.github.com>
2022-11-28 19:50:59 -0600

    Add files via upload

--- CetisTech_parcial3/entro/index.php
+++ CetisTech_parcial3/entro/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BonIce</title>
+    <title>Cetis Tecnologia</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <script src="js/jquery-3.6.1.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
